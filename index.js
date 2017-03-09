df0e3f84fa0d2e1b60f90209ba4d62ae35ad01b2
Tyler.Austgen <taustgen@gmail.com>
2017-03-09 10:45:58 -0800

    Beautified code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#butt").click(function(){
-    var message = {
-      username: $('#user').val(),
-      message: $('#message').val()
-    };
-    //var values = message.serialize();
-    alert(message);
-    $.ajax({
-      type:'POST',
-      url:'http://originmessages.herokuapp.com/messages',
-      data: JSON.stringify(message),
-      contentType: "application/json",
-      dataType:"json"
+    $("#butt").click(function() {
+        var message = {
+            username: $('#user').val(),
+            message: $('#message').val()
+        };
+        alert(message);
+        $.ajax({
+            type: 'POST',
+            url: 'http://originmessages.herokuapp.com/messages',
+            data: JSON.stringify(message),
+            contentType: "application/json",
+            dataType: "json"
+        });
     });
-  });
 });
